84ee8ba102b177226bffd2f79be0a644d07d8e8e
Kyle Foundry <kylefoundry@gmail.com>
2021-06-25 13:43:42 -0500

    added prefix to css variables

--- src/components/paymentDetails.css.ts
+++ src/components/paymentDetails.css.ts
@@ -7,7 +7,7 @@ CanopyUI consumers provide CSS variables for the given components like the follo
 
 <style>
     cnpy-payment-details {
-        --cnpy-primary-color: darkblue;
+        --cui-cnpy-primary-color: darkblue;
     }
 </style>
 
@@ -18,69 +18,69 @@ export const paymentDetailsCSS = html`
 <style>
   :root {
     /* Colors */
-    --color-primary:            #4867FF;
-    --color-secondary:          #00C4FF;
-    --color-success:            #00BD7E;
-    --color-warning:            #FA8C16;
-    --color-danger:             #FF434E;
-    --color-black:              #05102E;
+    --cui-color-primary:            #4867FF;
+    --cui-color-secondary:          #00C4FF;
+    --cui-color-success:            #00BD7E;
+    --cui-color-warning:            #FA8C16;
+    --cui-color-danger:             #FF434E;
+    --cui-color-black:              #05102E;
   
-    --background-color-light:   #ffffff;
-    --background-color-dark:    #0C1B45;
-    --background-color-faded:   #FBFCFF;
-    --text-color-body:          #3E4E7A;
-    --text-color-headers:       #0C1B45;
-    --text-color-light:         #C9CEDE;
+    --cui-background-color-light:   #ffffff;
+    --cui-background-color-dark:    #0C1B45;
+    --cui-background-color-faded:   #FBFCFF;
+    --cui-text-color-body:          #3E4E7A;
+    --cui-text-color-headers:       #0C1B45;
+    --cui-text-color-light:         #C9CEDE;
   
   
     /* Typography */
-    --font-family-base:         -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    --font-family-headers:      var(--font-family-base);
-    --font-size-base:           1rem;
-    --font-size-xs:             .75rem;
-    --font-size-sm:             .875rem;
-    --font-size-lg:             1.25rem;
-    --font-size-xl:             1.5rem;
-    --font-size-h4:             2rem;
-    --font-size-h3:             2.5rem;
-    --font-size-h2:             3rem;
-    --font-size-h1:             4rem;
-    --font-weight-bold:         700;
-    --font-weight-semibold:     500;
+    --cui-font-family-base:         -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    --cui-font-family-headers:      var(--cui-font-family-base);
+    --cui-font-size-base:           1rem;
+    --cui-font-size-xs:             .75rem;
+    --cui-font-size-sm:             .875rem;
+    --cui-font-size-lg:             1.25rem;
+    --cui-font-size-xl:             1.5rem;
+    --cui-font-size-h4:             2rem;
+    --cui-font-size-h3:             2.5rem;
+    --cui-font-size-h2:             3rem;
+    --cui-font-size-h1:             4rem;
+    --cui-font-weight-bold:         700;
+    --cui-font-weight-semibold:     500;
   
     /* Border Radius */
-    --border-radius:            16px;
-    --border-radius-sm:         8px;
+    --cui-border-radius:            16px;
+    --cui-border-radius-sm:         8px;
 
     /* Borders */
-    --border-width:             1px;
-    --border-color:             #E1E4F2;
-    --border-style:             solid;
+    --cui-border-width:             1px;
+    --cui-border-color:             #E1E4F2;
+    --cui-border-style:             solid;
   
     /* Box Shadow */
-    --box-shadow:               0px 4px 16px rgba(5, 16, 46, 0.04);
-    --box-shadow-lg:            0px 6px 32px rgba(5, 16, 46, 0.04);
+    --cui-box-shadow:               0px 4px 16px rgba(5, 16, 46, 0.04);
+    --cui-box-shadow-lg:            0px 6px 32px rgba(5, 16, 46, 0.04);
   
     /* Spacing */
-    --spacing-1:                  4px;
-    --spacing-2:                  8px;
-    --spacing-3:                  16px;
-    --spacing-4:                  24px;
-    --spacing-5:                  32px;
-    --spacing-6:                  48px;
-    --spacing-7:                  56px;
-    --spacing-8:                  64px;
-    --spacing-9:                  72px;
+    --cui-spacing-1:                  4px;
+    --cui-spacing-2:                  8px;
+    --cui-spacing-3:                  16px;
+    --cui-spacing-4:                  24px;
+    --cui-spacing-5:                  32px;
+    --cui-spacing-6:                  48px;
+    --cui-spacing-7:                  56px;
+    --cui-spacing-8:                  64px;
+    --cui-spacing-9:                  72px;
   }
 
   cnpy-payment-details {
-    background-color: var(--background-color-light);
-    border-radius: var(--border-radius);
-    box-shadow: var(--box-shadow-lg);
-    color: var(--text-color-body);
+    background-color: var(--cui-background-color-light);
+    border-radius: var(--cui-border-radius);
+    box-shadow: var(--cui-box-shadow-lg);
+    color: var(--cui-text-color-body);
     display: block;
-    font-family: var(--font-family-base);
-    font-size: var(--font-size-base);
+    font-family: var(--cui-font-family-base);
+    font-size: var(--cui-font-size-base);
     width: 900px; /* TODO: Make Responsive */
   }
 
@@ -90,15 +90,15 @@ export const paymentDetailsCSS = html`
   }
 
   cnpy-stat span[part="label"] {
-    font-size: var(--font-size-sm);
-    margin-bottom: var(--spacing-1);
+    font-size: var(--cui-font-size-sm);
+    margin-bottom: var(--cui-spacing-1);
   }
 
   cnpy-stat span[part="value"] {
-    color: var(--color-primary) !important;
-    font-size: var(--font-size-h4);
-    font-weight: var(--font-weight-bold);
-    color: var(--cnpy-primary-color);
+    color: var(--cui-color-primary) !important;
+    font-size: var(--cui-font-size-h4);
+    font-weight: var(--cui-font-weight-bold);
+    color: var(--cui-cnpy-primary-color);
   }
 
   cnpy-stat-sm {
@@ -107,30 +107,30 @@ export const paymentDetailsCSS = html`
   }
 
   cnpy-stat-sm span[part="label"] {
-    font-size: var(--font-size-xs);
-    margin-bottom: var(--spacing-1);
+    font-size: var(--cui-font-size-xs);
+    margin-bottom: var(--cui-spacing-1);
   }
 
   cnpy-stat-sm span[part="value"] {
-    color: var(--text-color-headers);
-    font-weight: var(--font-weight-semibold);
+    color: var(--cui-text-color-headers);
+    font-weight: var(--cui-font-weight-semibold);
   }
 
   slot[name="top"] {
-    border-bottom: var(--border-width) var(--border-color) var(--border-style);
+    border-bottom: var(--cui-border-width) var(--cui-border-color) var(--cui-border-style);
     display: grid;
     grid-template-columns: 25% 25% 25% 25%;
     grid-template-rows: auto; 
-    column-gap: var(--spacing-4);
-    padding: var(--spacing-5);
+    column-gap: var(--cui-spacing-4);
+    padding: var(--cui-spacing-5);
   }
 
   slot[name="bottom"] {
     display: grid;
     grid-template-columns: 25% 25% 25% 25%;
     grid-template-rows: auto; 
-    column-gap: var(--spacing-4);
-    padding: var(--spacing-4) var(--spacing-5);
+    column-gap: var(--cui-spacing-4);
+    padding: var(--cui-spacing-4) var(--cui-spacing-5);
   }
 </style>
 `;
