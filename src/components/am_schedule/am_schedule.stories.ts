e5a1b7fe848d692f281808b7faf9107e0add1b04
Kyle Foundry <kylefoundry@gmail.com>
2021-07-01 19:50:05 -0500

    updated storybook data for am schedule

--- src/components/am_schedule/am_schedule.stories.ts
+++ src/components/am_schedule/am_schedule.stories.ts
@@ -5,6 +5,9 @@ import { CSSTemplateVars } from "../../variables.css";
 
 export default {
   title: "Components/AmSchedule",
+  parameters:{
+    layout: 'padded',
+  },
 };
 
 const someAmItems: AmScheduleItemsProp = [
@@ -13,7 +16,7 @@ const someAmItems: AmScheduleItemsProp = [
     "cycle_exclusive_end": "2018-07-20T09:10:14+00:00",
     "min_pay_due_at": "2018-07-20T09:10:14+00:00",
     "am_min_pay_cents": 84000,
-    "am_cycle_payment_cents": 18325,
+    "am_cycle_payment_cents": 84000,
     "am_interest_cents": 10000,
     "am_deferred_cents": 4000,
     "am_principal_cents": 70000,
@@ -29,6 +32,21 @@ const someAmItems: AmScheduleItemsProp = [
     "min_pay_due_at": "2018-08-20T09:10:14+00:00",
     "am_min_pay_cents": 84000,
     "am_cycle_payment_cents": 18325,
+    "am_interest_cents": 10000,
+    "am_deferred_cents": 4000,
+    "am_principal_cents": 70000,
+    "am_start_principal_balance_cents": 136691,
+    "am_end_principal_balance_cents": 136691,
+    "am_start_total_balance_cents": 136691,
+    "am_end_total_balance_cents": 136691,
+    "paid_on_time": true
+  },
+  {
+    "line_item_id": 401,
+    "cycle_exclusive_end": "2018-09-20T09:10:14+00:00",
+    "min_pay_due_at": "2018-09-20T09:10:14+00:00",
+    "am_min_pay_cents": 84000,
+    "am_cycle_payment_cents": 0,
     "am_interest_cents": 10000,
     "am_deferred_cents": 4000,
     "am_principal_cents": 70000,
