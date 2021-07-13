0c9d304a8814f64423665119621233971bde067a
kpamaran <kevpamaran@gmail.com>
2021-07-12 19:31:34 -0700

    Update AccountOverview API

--- src/components/account_overview/account_overview.ts
+++ src/components/account_overview/account_overview.ts
@@ -3,11 +3,11 @@ import { customElement, property } from "lit/decorators.js";
 import { paymentDetailsCSS } from "./acount_overview.css";
 
 export interface AccountOverviewPropDetails {
-  amount: number;
-  credit_limit: number;
-  available_credit: number;
+  total_balance_cents: number;
+  credit_limit_cents: number;
+  available_credit_cents: number;
   pending_charges: number;
-  promo_exp: string
+  promo_exclusive_end: string
 }
 
 @customElement("cui-account-overview")
@@ -16,26 +16,26 @@ export class AccountOverview extends LitElement {
 
   @property({ attribute: 'details', type: Object })
   public details: AccountOverviewPropDetails = {
-    amount: 0,
-    credit_limit: 0,
-    available_credit: 0,
+    total_balance_cents: 0,
+    credit_limit_cents: 0,
+    available_credit_cents: 0,
     pending_charges: 0,
-    promo_exp: "-"
+    promo_exclusive_end: "-"
   }
 
   render(): TemplateResult<1> {
 
     // Appears if no element with attr slot "top" given.
     const defaultTop = html`
-      <cui-stat size="large" label="Amount" value="${this.details?.amount}" currency=true></cui-stat>
-      <cui-stat size="large" label="Credit Limit" value="${this.details?.credit_limit}" currency=true></cui-stat>
+      <cui-stat size="large" label="Current Balance" value="${this.details?.total_balance_cents}" currency=true></cui-stat>
+      <cui-stat size="large" label="Credit Limit" value="${this.details?.credit_limit_cents}" currency=true></cui-stat>
     `;
 
     // Appears if no element with attr slot "bottom" given.
     const defaultBottom = html`
-      <cui-stat size="small" label="Available Credit" value="${this.details?.available_credit}" currency=true></cui-stat>
+      <cui-stat size="small" label="Available Credit" value="${this.details?.available_credit_cents}" currency=true></cui-stat>
       <cui-stat size="small" label="Pending Charges" value="${this.details?.pending_charges}" currency=true></cui-stat>
-      <cui-stat size="small" label="Promo Period Expiration" value="${this.details?.promo_exp}"></cui-stat>
+      <cui-stat size="small" label="Promo Period Expiration" value="${this.details?.promo_exclusive_end}"></cui-stat>
     `
 
     return html`
