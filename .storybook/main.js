b67a940d690a68c71531ffeda511b2acba4a41ff
kpamaran <kevpamaran@gmail.com>
2021-07-12 18:38:09 -0700

    Switch story format from CSF to MDX

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(ts|mdx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-docs",
+    "@storybook/addon-essentials",
+    "@storybook/addon-links"
   ]
 }
