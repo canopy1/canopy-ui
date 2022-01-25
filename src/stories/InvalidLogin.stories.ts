61fdd8fde0c2dc4d059b5b9d8a10048db05848c2
Aaron Maturen <aaron@canopyservicing.com>
2022-01-25 16:11:10 -0500

    docs: add credentials and invalid error

---
+++ src/stories/InvalidLogin.stories.ts
@@ -0,0 +1,186 @@
+import { linkTo } from "@storybook/addon-links";
+import { html, css, render } from "lit";
+import { dashboardLogoSVG } from "../icons/inline";
+
+const demoDashboardCSS = css`
+  #DashboardDemo {
+    align-items: center;
+    background-color: #fff;
+    color: var(--cui-text-color-body);
+    display: flex;
+    font-family: var(--cui-font-family-base);
+    font-size: var(--cui-font-size-base);
+    justify-content: center;
+    line-height: var(--cui-line-height-base);
+    min-height: calc(100vh - 64px);
+    padding: 32px 16px;
+    width: calc(100% - 32px);
+  }
+
+  a {
+    color: var(--cui-color-primary);
+    display: inline-block;
+    padding: 4px;
+    text-decoration: none;
+  }
+
+  h4 {
+    color: var(--cui-text-color-headers);
+    font-size: var(--cui-font-size-h4);
+    margin-bottom: 16px;
+    margin-top: 0;
+  }
+
+  .auth-container {
+    max-width: 320px;
+    text-align: center;
+    width: 100%;
+  }
+
+  .auth-container cui-card {
+    margin-bottom: 24px;
+    padding: 16px 24px 24px;
+    width: calc(100% - 48px);
+  }
+
+  .logo-container {
+    text-align: center;
+    margin-bottom: 32px;
+  }
+
+  cui-input-text {
+    margin-bottom: 16px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    #DashboardDemo {
+    }
+  }
+
+  @media only screen and (min-width: 992px) {
+    #DashboardDemo {
+      grid-template-columns: 1fr 2fr;
+    }
+
+    .main .header {
+      padding: 0 48px;
+    }
+
+    .main .content {
+      padding: 48px;
+    }
+  }
+
+  @media only screen and (min-width: 1400px) {
+    #DashboardDemo {
+      grid-template-columns: 1fr 3fr;
+    }
+  }
+`;
+
+export default {
+  title: "Demos/InvalidLogin",
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {
+    PrimaryColor: {
+      control: { type: "color" },
+    },
+    ButtonHoverColor: {
+      control: { type: "color" },
+    },
+    AltBackgroundColor: {
+      control: { type: "color" },
+    },
+    ContainerBorderRadius: {
+      control: { type: "number" },
+    },
+    ButtonBorderRadius: {
+      control: { type: "number" },
+    },
+    Logo: {
+      control: { type: "file" },
+    },
+  },
+};
+
+const Template = ({
+  PrimaryColor,
+  ContainerBorderRadius,
+  AltBackgroundColor,
+  ButtonBorderRadius,
+  ButtonHoverColor,
+  Logo = dashboardLogoSVG,
+}) => {
+
+  let email = "";
+  let password = "";
+
+  const handleClick = (e) => {
+    console.log(email, password)
+
+    if (email === "hello@flexport.com" && password === "password") {
+      linkTo("Demos/Dashboard", "Multiloan Account V 2")();
+      return;
+    }
+
+    linkTo("Demos/InvalidLogin", "Invalid Login")();
+  }
+
+  return html`
+    <style>${demoDashboardCSS}</style>
+    <style>
+      :root {
+        --cui-color-primary: ${PrimaryColor};
+        --cui-btn-background-color-hover: ${ButtonHoverColor};
+        --cui-border-radius: ${ContainerBorderRadius}px;
+        --cui-btn-border-radius: ${ButtonBorderRadius}px;
+      }
+
+      #DashboardDemo {
+        background-color: ${AltBackgroundColor};
+      }
+    </style>
+    <div id="DashboardDemo">
+      <div class="auth-container">
+        <div class="logo-container">
+          ${Logo}
+        </div>
+        <cui-card>
+          <h4>Account Login</h4>
+          <div class="error">Invalid username or password.</div>
+          <cui-input-text
+            name="email"
+            value="${email}"
+            placeholder="Email Address"
+            @change="${e => { email = e.detail.value; }}"
+            required
+          >
+          </cui-input-text>
+          <cui-input-text
+            type="password"
+            name="password"
+            value="${password}"
+            @change="${e => { password = e.detail.value; }}"
+            placeholder="Password"
+            required
+          >
+          </cui-input-text>
+          <cui-btn @click=${(e) => handleClick(e)} >Login</cui-btn>
+        </cui-card>
+        <a data-sb-kind="Demos" data-sb-story="Register" href="#">Register A New Account</a><br />
+        <a data-sb-kind="Demos" data-sb-story="Password Reminder"href="#">Forgot Password?</a>
+      </div>
+    </div>
+  `;
+};
+
+export const InvalidLogin = Template.bind({});
+InvalidLogin.args = {
+  PrimaryColor: "#4867FF",
+  ButtonHoverColor: "#443CF8",
+  AltBackgroundColor: "#F2F5FD",
+  ContainerBorderRadius: 16,
+  ButtonBorderRadius: 8,
+};
