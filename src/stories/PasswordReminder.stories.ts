15e16a8b54ed8230c389515f37b791d896d59e9a
Kyle Foundry <kylefoundry@gmail.com>
2022-01-25 20:12:02 -0600

    updated flexport styling

--- src/stories/PasswordReminder.stories.ts
+++ src/stories/PasswordReminder.stories.ts
@@ -1,135 +1,65 @@
 import { linkTo } from "@storybook/addon-links";
 import { html, css } from "lit";
-import { dashboardLogoSVG } from "../icons/inline";
-import { defaults } from './defaults';
-
-const demoDashboardCSS = css`
-  #DashboardDemo {
-    align-items: center;
-    background-color: #fff;
-    color: var(--cui-text-color-body);
-    display: flex;
-    font-family: var(--cui-font-family-base);
-    font-size: var(--cui-font-size-base);
-    justify-content: center;
-    line-height: var(--cui-line-height-base);
-    min-height: calc(100vh - 64px);
-    padding: 32px 16px;
-    width: calc(100% - 32px);
-  }
-
-  a {
-    color: var(--cui-color-primary);
-    display: inline-block;
-    padding: 4px;
-    text-decoration: none;
-  }
-
-  img {
-    width: 150px;
-  }
-
-  h4 {
-    color: var(--cui-text-color-headers);
-    font-size: var(--cui-font-size-h4);
-    margin-bottom: 16px;
-    margin-top: 0;
-  }
-
-  .auth-container {
-    max-width: 320px;
-    text-align: center;
-    width: 100%;
-  }
-
-  .auth-container cui-card {
-    margin-bottom: 24px;
-    padding: 16px 24px 24px;
-    width: calc(100% - 48px);
-  }
-
-  .logo-container {
-    text-align: center;
-    margin-bottom: 32px;
-  }
-
-  cui-input-text {
-    margin-bottom: 16px;
-  }
-
-  @media only screen and (min-width: 768px) {
-    #DashboardDemo {
-    }
-  }
-
-  @media only screen and (min-width: 992px) {
-    #DashboardDemo {
-      grid-template-columns: 1fr 2fr;
-    }
-
-    .main .header {
-      padding: 0 48px;
-    }
-
-    .main .content {
-      padding: 48px;
-    }
-  }
-
-  @media only screen and (min-width: 1400px) {
-    #DashboardDemo {
-      grid-template-columns: 1fr 3fr;
-    }
-  }
-`;
+import { flexportVars, defaultControls, demoDashboardCSS } from './defaults';
 
 export default {
-  title: "Demos",
+  title: "Flexport Demo/Forgot Password",
   parameters: {
     layout: "fullscreen",
   },
   argTypes: {
-    PrimaryColor: {
-      control: { type: "color" },
-    },
-    ButtonHoverColor: {
-      control: { type: "color" },
-    },
-    AltBackgroundColor: {
-      control: { type: "color" },
-    },
-    ContainerBorderRadius: {
-      control: { type: "number" },
-    },
-    ButtonBorderRadius: {
-      control: { type: "number" },
-    },
-    logoUrl: {
-      control: { type: "string" },
-    },
+    ...defaultControls
   },
 };
 
 const Template = ({
-  PrimaryColor,
-  ContainerBorderRadius,
   AltBackgroundColor,
+  AuthBgImgUrl,
+  BaseFontSize,
   ButtonBorderRadius,
+  ButtonHeight,
   ButtonHoverColor,
+  ContainerBorderRadius,
+  InputBorderColor,
+  InputBorderWidth,
+  InputHeight,
+  InputPaddingHorizontal,
+  InputPlaceholderTextColor,
+  InputBorderFocusColor,
   logoUrl,
+  PrimaryColor,
+  TextBody,
+  TextDark,
 }) => {
   return html`
     <style>${demoDashboardCSS}</style>
     <style>
       :root {
+        --cui-text-color-body: ${TextBody};
         --cui-color-primary: ${PrimaryColor};
         --cui-btn-background-color-hover: ${ButtonHoverColor};
         --cui-border-radius: ${ContainerBorderRadius}px;
         --cui-btn-border-radius: ${ButtonBorderRadius}px;
+        --cui-btn-height: ${ButtonHeight}px;
+        --cui-input-border-radius: var(--cui-btn-border-radius);
+        --cui-color-black: ${TextDark};
+        --cui-text-color-headers: var(--cui-color-black);
+        --cui-btn-text-color: var(--cui-color-black);
+        --cui-input-border-color: ${InputBorderColor};
+        --cui-input-border-color-active: ${InputBorderFocusColor};
+        --cui-input-border-width: ${InputBorderWidth}px;
+        --cui-font-size-base: ${BaseFontSize}px;
+        --cui-input-padding-horizontal: ${InputPaddingHorizontal}px;
+        --cui-input-height: ${InputHeight}px;
+        --cui-input-placeholder-color: ${InputPlaceholderTextColor};
       }
 
       #DashboardDemo {
         background-color: ${AltBackgroundColor};
+        background-image: url(${AuthBgImgUrl});
+        background-size: 80%;
+        background-position: bottom right;
+        background-repeat: no-repeat;
       }
     </style>
     <div id="DashboardDemo">
@@ -153,7 +83,7 @@ const Template = ({
   `;
 };
 
-export const PasswordReminder = Template.bind({});
-PasswordReminder.args = {
-  ...defaults,
+export const ForgotPassword = Template.bind({});
+ForgotPassword.args = {
+  ...flexportVars,
 };
--- src/stories/defaults.ts
+++ src/stories/defaults.ts
@@ -1,9 +1,162 @@
-export const defaults = {
-    PrimaryColor: "#4867FF",
-    ButtonHoverColor: "#443CF8",
-    AltBackgroundColor: "#F2F5FD",
-    MobileHeaderColor: "68ADFF",
-    ContainerBorderRadius: 16,
-    ButtonBorderRadius: 8,
-    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Flexport_logo.svg/440px-Flexport_logo.svg.png",
+import { css } from "lit";
+
+export const flexportVars = {
+  BaseFontSize: 16,
+  PrimaryColor: "#45DABE",
+  ButtonHeight: 56,
+  ButtonHoverColor: "#27C5A7",
+  AltBackgroundColor: "#24303E",
+  TextBody: "#4D5563",
+  TextDark: "#24303E",
+  InputPlaceholderTextColor: "#bac1ca",
+  InputBorderColor: "#6A768B",
+  InputBorderFocusColor: "#24303E",
+  InputBorderWidth: 2,
+  InputHeight: 53,
+  InputPaddingHorizontal: 20,
+  InputPaddingVertical: 13,
+  MobileHeaderColor: "68ADFF",
+  ContainerBorderRadius: 0,
+  ButtonBorderRadius: 5,
+  logoUrl: "http://retinab2.com/wp-content/uploads/2020/06/flexport-logow-r.png",
+  AuthBgImgUrl: "https://www.flexport.com/static/signup-flow-background-decoration-a381d1e4863be9471489267b3a88c92f.png",
 }
+
+export const defaultControls = {
+  PrimaryColor: {
+    control: { type: "color" },
+  },
+  BaseFontSize: {
+    control: { type: "number" },
+  },
+  ButtonHeight: {
+    control: { type: "number" },
+  },
+  ButtonHoverColor: {
+    control: { type: "color" },
+  },
+  AltBackgroundColor: {
+    control: { type: "color" },
+  },
+  TextBody: {
+    control: { type: "color" },
+  },
+  TextDark: {
+    control: { type: "color" },
+  },
+  InputBorderColor: {
+    control: { type: "color" },
+  },
+  InputBorderFocusColor: {
+    control: { type: "color" },
+  },
+  InputBorderWidth: {
+    control: { type: "number" },
+  },
+  InputHeight: {
+    control: { type: "number" },
+  },
+  InputPaddingHorizontal: {
+    control: { type: "number" },
+  },
+  InputPlaceholderTextColor: {
+    control: { type: "color" },
+  },
+  ContainerBorderRadius: {
+    control: { type: "number" },
+  },
+  ButtonBorderRadius: {
+    control: { type: "number" },
+  },
+  logoUrl: {
+    control: { type: "string" },
+  },
+  AuthBgImgUrl: {
+    control: { type: "string" },
+  },
+};
+
+export const demoDashboardCSS = css`
+  #DashboardDemo {
+    align-items: center;
+    background-color: #fff;
+    color: var(--cui-text-color-body);
+    display: flex;
+    font-family: var(--cui-font-family-base);
+    font-size: var(--cui-font-size-base);
+    justify-content: center;
+    line-height: var(--cui-line-height-base);
+    min-height: calc(100vh - 64px);
+    padding: 32px 16px;
+    width: calc(100% - 32px);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a {
+    color: var(--cui-color-primary);
+    display: inline-block;
+    padding: 4px;
+    text-decoration: none;
+  }
+
+  img {
+    width: 150px;
+  }
+
+  h4 {
+    color: var(--cui-text-color-headers);
+    font-size: var(--cui-font-size-h4);
+    margin-bottom: 40px;
+    margin-top: 0;
+  }
+
+  .auth-container {
+    max-width: 400px;
+    text-align: center;
+    width: 100%;
+  }
+
+  .auth-container cui-card {
+    margin-bottom: 24px;
+    padding: 16px 24px 24px;
+    width: calc(100% - 48px);
+  }
+
+  .logo-container {
+    text-align: center;
+    margin-bottom: 32px;
+  }
+
+  cui-input-text {
+    margin-bottom: 16px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .auth-container cui-card {
+      margin-bottom: 24px;
+      padding: 40px 60px 60px;
+      width: calc(100% - 120px);
+    }
+  }
+
+  @media only screen and (min-width: 992px) {
+    #DashboardDemo {
+      grid-template-columns: 1fr 2fr;
+    }
+
+    .main .header {
+      padding: 0 48px;
+    }
+
+    .main .content {
+      padding: 48px;
+    }
+  }
+
+  @media only screen and (min-width: 1400px) {
+    #DashboardDemo {
+      grid-template-columns: 1fr 3fr;
+    }
+  }
+`;
